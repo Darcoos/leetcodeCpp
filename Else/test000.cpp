100eecdc275e855c178d17837dbcee4958f66c16
Darcoos <2670604162@qq.com>
2024-02-05 15:48:08 +0800

    config

---
+++ Else/test000.cpp
@@ -0,0 +1,22 @@
+/*
+  @File    :   test000.cpp
+  @Folder  :   Else\test000.cpp
+  @Time    :   2024/02/05 15:46:34
+  @Author  :   »¨³ÇÎáÃû
+  @Contact :   2670604162@qq.com
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+
+    system("pause");
+    return 0;
+}
---
+++ Leetcode/454.cpp
@@ -0,0 +1,22 @@
+/*
+  @File    :   454.cpp
+  @Folder  :   Leetcode\454.cpp
+  @Time    :   2024/02/05 15:47:03
+  @Author  :   ª®≥«Œ·√˚
+  @Contact :   2670604162@qq.com
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+
+    system("pause");
+    return 0;
+}
